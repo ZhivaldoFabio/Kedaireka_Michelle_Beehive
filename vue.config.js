0e56a928e144b5f665d653a7a4ac1953b09587e2
Zhivaldo <79153900+ZhivaldoFabio@users.noreply.github.com>
2023-05-24 14:37:32 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -12,3 +12,25 @@ module.exports = defineConfig({
 		}
   }
 })
+
+// module.exports = {
+//   devServer: {
+//     allowedHosts: [
+//       'localhost',
+//       'example.com',
+//       '192.168.1.1',
+//     ],
+//   },
+// };
+
+// module.exports = {
+//   devServer: {
+//     disableHostCheck: true
+//   }
+// }
+
+// module.exports = {
+//   devServer: {
+//     public: 'subdomain.domain.ext:port'
+//   }
+// };
